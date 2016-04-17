9b4029e73e6a553e34611a742b7294fc2badfb63
keke2014 <zhangjuke@qq.com>
2016-04-17 23:41:40 +0800

    add gradle+springmvc project

---
+++ gradle-taste-test/gradle-taste-web/src/main/java/controller/LoginCommand.java
@@ -0,0 +1,12 @@
+package controller;
+
+import lombok.Data;
+
+/**
+ * Created by juke.zjk on 2016/4/17 017.
+ */
+@Data
+public class LoginCommand {
+    private String userName;
+    private String password;
+}
---
+++ gradle-taste-test/gradle-taste-web/src/main/java/controller/LoginController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login.html")
+    public String loginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginCheck.html")
+    public ModelAndView loginCheck(HttpServletRequest request, LoginCommand loginCommand) {
+        ModelAndView modelAndView = new ModelAndView("main");
+        modelAndView.addObject("user", loginCommand);
+        return modelAndView;
+    }
+}
